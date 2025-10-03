31ac69fd68d9aedd52843f9ed40a92315739d7b8
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-10-03 18:20:10 +0530

    fix-tab

--- 10.0/Apps/DeveloperBalance/Pages/Controls/TaskView.xaml.cs
+++ 10.0/Apps/DeveloperBalance/Pages/Controls/TaskView.xaml.cs
@@ -5,34 +5,73 @@ namespace DeveloperBalance.Pages.Controls;
 
 public partial class TaskView
 {
-	public TaskView()
-	{
-		InitializeComponent();
-	}
+    public TaskView()
+    {
+        InitializeComponent();
+#if WINDOWS
+        TaskViewBorder.Loaded += OnTaskViewLoaded;
+#endif
+    }
 
-	public static readonly BindableProperty TaskCompletedCommandProperty = BindableProperty.Create(
-		nameof(TaskCompletedCommand),
-		typeof(ICommand),
-		typeof(TaskView),
-		null);
+#if WINDOWS
 
-	public ICommand TaskCompletedCommand
-	{
-		get => (ICommand)GetValue(TaskCompletedCommandProperty);
-		set => SetValue(TaskCompletedCommandProperty, value);
-	}
+    private void OnTaskViewLoaded(object sender, EventArgs e)
+    {
+        if (TaskViewBorder.Handler?.PlatformView is Microsoft.UI.Xaml.FrameworkElement platformView)
+        {
+            platformView.IsTabStop = true;
+			platformView.UseSystemFocusVisuals = true;
+            platformView.KeyDown += OnKeyDown;
+        }
+    }
+ 
+    private void OnKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Space || e.Key == Windows.System.VirtualKey.Enter)
+        {
+            if (BindingContext is ProjectTask task)
+            {
+                Element? current = this.Parent;
+                while (current != null)
+                {
+                    if (current.BindingContext is IProjectTaskPageModel pageModel)
+                    {
+                        pageModel.NavigateToTaskCommand?.Execute(task);
+                        break;
+                    }
 
-	private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
-	{
-		var checkbox = (CheckBox)sender;
-		
-		if (checkbox.BindingContext is not ProjectTask task)
-			return;
-		
-		if (task.IsCompleted == e.Value)
-			return;
+                    current = current.Parent;
+                }
+            }
+            e.Handled = true;
+        }
+    }
 
-		task.IsCompleted = e.Value;
-		TaskCompletedCommand?.Execute(task);
-	}
+#endif
+
+    public static readonly BindableProperty TaskCompletedCommandProperty = BindableProperty.Create(
+        nameof(TaskCompletedCommand),
+        typeof(ICommand),
+        typeof(TaskView),
+        null);
+
+    public ICommand TaskCompletedCommand
+    {
+        get => (ICommand)GetValue(TaskCompletedCommandProperty);
+        set => SetValue(TaskCompletedCommandProperty, value);
+    }
+
+    private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var checkbox = (CheckBox)sender;
+
+        if (checkbox.BindingContext is not ProjectTask task)
+            return;
+
+        if (task.IsCompleted == e.Value)
+            return;
+
+        task.IsCompleted = e.Value;
+        TaskCompletedCommand?.Execute(task);
+    }
 }
